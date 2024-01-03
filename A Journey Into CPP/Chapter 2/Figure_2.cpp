f454b193e080d09dd1f8db17a7f1f6bc69adb97d
Moises Moreno <it.moisesmoreno@gmail.com>
2024-01-03 15:22:24 -0600

    Added Chapter 2 snippets

---
+++ A Journey Into CPP/Chapter 2/Figure_2.cpp
@@ -0,0 +1,16 @@
+// Fig. 2
+#include <iostream>
+
+int main() {
+    int count = 5;
+
+    // Use the while statement for iterative counting
+    while (count > 0) {
+        std::cout << count << " ";
+        count--;
+    }
+
+    std::cout << "Liftoff!" << std::endl;
+
+    return 0;
+}
